46acd918997df4ac29dfa619d63fae79cea46413
Administrator <564328226@qq.com>
2021-01-05 17:52:33 +0800

    2222

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -3,12 +3,15 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @RestController
 @RequestMapping("/login")
 public class HelloWorld {
     @RequestMapping (value = "/hello",method = RequestMethod.GET)
     public String sayHello(){
+
         return "HelloWorld!!!!";
     }
+
 }
