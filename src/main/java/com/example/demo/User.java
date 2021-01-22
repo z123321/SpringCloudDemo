88c78a7edfcb147d7a7878a459bb278835cb9c8d
Administrator <564328226@qq.com>
2021-01-22 14:45:18 +0800

    1111

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -7,11 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
-public class HelloWorld {
+public class User {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String sayHello() {
-        return "HelloWorld!!!!";
-    }
+
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login() {
